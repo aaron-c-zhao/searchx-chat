e0fc201b8184e3b0b24c7a8d1009d797c7870704
aaronzhao <aaron.zhaocr@gmail.com>
2021-06-19 10:43:40 +0200

    add support for open url in a viewpage

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import Message from './Messages';
+import React, { Component } from "react";
+import Message from "./Messages";
 
 class MessageList extends Component {
-
   componentDidUpdate(_prevProps, _prevState) {
     this.scrollList.scrollTop = this.scrollList.scrollHeight;
   }
 
-  render () {
+  render() {
     return (
-      <div className="sc-message-list" ref={el => this.scrollList = el}>
+      <div className="sc-message-list" ref={(el) => (this.scrollList = el)}>
         {this.props.messages.map((message, i) => {
-          return <Message 
-            message={message} 
-            key={i} 
-            yes={this.props.yes}
-            no={this.props.no}/>;
+          return (
+            <Message
+              message={message}
+              key={i}
+              yes={this.props.yes}
+              no={this.props.no}
+              openUrl={this.props.openUrl}
+            />
+          );
         })}
-      </div>);
+      </div>
+    );
   }
 }
 
